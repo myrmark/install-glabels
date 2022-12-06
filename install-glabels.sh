0f84d197404048d3c7a9d878765a35db817b4615
Filip Malmberg <91657147+myrmark@users.noreply.github.com>
2022-12-06 13:19:13 +0100

    Update install-glabels.sh

--- install-glabels.sh
+++ install-glabels.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-git clone https://github.com/myrmark/glabels-qt
+cd
 
-sudo apt install cmake
-sudo apt install qtbase5-dev libqt5svg5-dev qttools5-dev zlib1g-dev
-sudo apt install pkgconf libqrencode-dev
+sudo apt install cmake -y
+sudo apt install qtbase5-dev libqt5svg5-dev qttools5-dev zlib1g-dev -y
+sudo apt install pkgconf libqrencode-dev -y
 
 wget https://downloads.sourceforge.net/project/zint/zint/2.6.3/zint-2.6.3_final.tar.gz
 tar xzf zint-2.6.3_final.tar.gz
 cd zint-2.6.3.src/
 mkdir build && cd build && cmake .. && make
 sudo make install
+
+cd
 
 wget https://ftp.gnu.org/gnu/barcode/barcode-0.98.tar.gz
 tar xzf barcode-0.98.tar.gz
@@ -19,6 +21,8 @@ cd barcode-0.98/
 sudo make install
 
 cd
+
+git clone https://github.com/myrmark/glabels-qt
 cd glabels-qt
 mkdir build
 cd build
